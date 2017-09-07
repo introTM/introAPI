a649486ffc620779eb4a8c8925301296c1056110
intro <31727276+introTM@users.noreply.github.com>
2017-09-07 18:07:09 +0430

    Update intro_API.sh

--- intro_API.sh
+++ intro_API.sh
@@ -25,20 +25,20 @@ install() {
 }
 
 function print_logo() {
-	green "          ____  ____     _____"
-	green "         |  _ )|  _ \   |_   _|___ ____   __  __"
-	green "         |  _ \| |_) )    | |/ .__|  _ \_|  \/  |"
-	green "         |____/|____/     |_|\____/\_____|_/\/\_|"
+	green "         omid Alireza"
+	green "         @iNtR0Team"
+	green "         sudo"
+	green "         iNtR0Team"
 	echo -e "\n\e[0m"
 }
 
 function logo_play() {
     declare -A txtlogo
     seconds="0.010"
-    txtlogo[1]=" ____  ____     _____"
-    txtlogo[2]="|  _ )|  _ \   |_   _|___ ____   __  __"
-    txtlogo[3]="|  _ \| |_) )    | |/ .__|  _ \_|  \/  |"
-    txtlogo[4]="|____/|____/     |_|\____/\_____|_/\/\_|"
+   txtlogo[1]="        iNtR0Team"
+    txtlogo[2]="       Sudo"
+    txtlogo[3]="      @iNtR0Team"
+    txtlogo[4]="     omid Alireza"
     printf "\e[31m\t"
     for i in ${!txtlogo[@]}; do
         for x in `seq 0 ${#txtlogo[$i]}`; do
@@ -58,7 +58,7 @@ function beyondteam() {
 	white "     >>>>                       We Are Programmer                               "
 	white "     >>>>                       We Are The Best                                 "
 	red   "     >>>>                       We Are Family                                   "
-	red   "     >>>>                       @BeyondTeam                                     "
+	red   "     >>>>                       @iNtR0Team                                     "
 	echo -e "\e[0m"
 }
 
